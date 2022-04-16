7560fdea98f74e0a00c15ccfe042c67a448dd438
0xKirz <kirzin06@gmail.com>
2022-04-16 20:03:49 +0800

    Add files via upload

---
+++ Portfolio/project.js
@@ -0,0 +1,24 @@
+let projects = [
+    {
+        name:"project one",
+        tags:"#html/css",
+        image: "project1.png",
+    },
+    {
+        name:"project two",
+        tags:"#javascript",
+        image: "project2.png",
+    },
+    {
+        name:"project three",
+        tags:"#html/css",
+        image: "project3.png",
+    },
+    {
+        name:"project Four",
+        tags:"#javascript",
+        image: "project4.png",
+    },
+    
+    
+]
